bbf7833af9ce73972c93d49ee4b5326ec1ab811d
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-05-08 21:48:35 -0700

    boilerplating

---
+++ src/main/java/us/drullk/craftydemocracy/CraftyDemocracyMod.java
@@ -0,0 +1,16 @@
+package us.drullk.craftydemocracy;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(CraftyDemocracyMod.MODID)
+public class CraftyDemocracyMod {
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "crafty_democracy";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
