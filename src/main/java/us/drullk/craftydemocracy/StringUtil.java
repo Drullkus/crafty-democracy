47f38ec65e75a360f9f00572969a70a8358fd7ed
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-05-30 16:37:31 -0700

    Fix whitespaces inside poll entries

--- src/main/java/us/drullk/craftydemocracy/StringUtil.java
+++ src/main/java/us/drullk/craftydemocracy/StringUtil.java
@@ -1,5 +1,8 @@
 package us.drullk.craftydemocracy;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
 import java.util.*;
 
 public class StringUtil {
@@ -8,8 +11,15 @@ public class StringUtil {
 		return str.matches("[A-Za-z0-9]+");
 	}
 
-	public static List<String> splitWhitespace(String str) {
-		return Arrays.asList(str.split("\\s+"));
+	public static List<String> splitWhitespace(String str) throws CommandSyntaxException {
+		StringReader reader = new StringReader(str);
+		List<String> result = new ArrayList<>();
+		while (reader.canRead()) {
+			reader.skipWhitespace();
+			if (!reader.canRead()) break;
+			result.add(reader.readString());
+		}
+		return result;
 	}
 
 }
--- src/main/java/us/drullk/craftydemocracy/polling/PollingCommands.java
+++ src/main/java/us/drullk/craftydemocracy/polling/PollingCommands.java
@@ -189,14 +189,14 @@ public class PollingCommands {
 			if (chosen.contains(choiceString)) {
 				UnaryOperator<Style> unvoteTrigger = s -> s
 						.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to remove vote for " + choiceString)))
-						.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/poll vote remove " + choiceString))
+						.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/poll vote remove \"" + choiceString + "\""))
 						;
 
 				line.append(Component.translatable(" [%s]", Component.literal("Unvote").withStyle(ChatFormatting.DARK_GREEN).withStyle(unvoteTrigger)));
 			} else if (choicesLeft > 0) {
 				UnaryOperator<Style> voteTrigger = s -> s
 						.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.literal("Click to add vote for " + choiceString)))
-						.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/poll vote add " + choiceString))
+						.withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/poll vote add \"" + choiceString + "\""))
 						;
 
 				line.append(Component.translatable(" [%s]", Component.literal("Vote").withStyle(ChatFormatting.GREEN).withStyle(voteTrigger)));
